84e8f0de6837d3938910ffa4ae27fb5cb65eab2f
Victor Alcântara <victoralcan@gmail.com>
2020-11-16 15:15:22 -0300

    Adicionando rota autenticado e não-autenticado. Adicionando novas dependências

--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Button, Text } from 'react-native';
 
 import { Container } from './styles';
 
-const Login: React.FC = () => {
+const Login: React.FC = (props) => {
   return (
     <Container>
       <Text>Login</Text>
+      <Button
+        title="loggin"
+        // @ts-ignore
+        onPress={() => props.navigation.navigate('LoggedIn')}
+      />
     </Container>
   );
 };
--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Ionicons, SimpleLineIcons, AntDesign, MaterialCommunityIcons } from '@expo/vector-icons';
+import { AntDesign, MaterialCommunityIcons, SimpleLineIcons } from '@expo/vector-icons';
 import colors from './styles/colors';
 
 import Home from './pages/Home';
@@ -10,95 +10,112 @@ import Disciplinas from './pages/Disciplinas';
 import Atividades from './pages/Atividades';
 import Notas from './pages/Notas';
 import Pessoas from './pages/Pessoas';
+import Login from './pages/Login';
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
-const Routes: React.FC = () => (
-    
-  <NavigationContainer>
-    <Navigator
-        tabBarOptions={{
-            style: { 
-                height: 70,
-                backgroundColor: colors.back,
-                borderTopWidth: 0,
+const Stack = createStackNavigator();
 
-            },
-            tabStyle: {
-                alignItems: 'center',
-                justifyContent: 'center',
-            },
-            iconStyle: {
-                flex: 0,
-                width: 20,
-                height: 20,
-            },
-            labelStyle: {
-                fontFamily: 'roboto_400',
-                fontSize: 11,
-                marginTop: 8,
-            },
-            inactiveTintColor: colors.black,
-            activeTintColor: colors.white
-        }}
-    >
-      <Screen name="Home" component={Home} options={{
-          tabBarIcon: ({ size, focused }) => {
-              return (
-                  <AntDesign
-                    name="home"
-                    size={size}
-                    color={focused ? colors.white : colors.black}
-                  />
-              )
-          }
-      }}/>
-      <Screen name="Disciplinas" component={Disciplinas} options={{
-          tabBarIcon: ({ size, focused }) => {
-              return (
-                  <SimpleLineIcons
-                    name="graduation"
-                    size={size}
-                    color={focused ? colors.white : colors.black}
-                  />
-              )
-          }
-      }}/>
-      <Screen name="Atividades" component={Atividades} options={{
-          tabBarIcon: ({ size, focused }) => {
-              return (
-                  <AntDesign
-                    name="edit"
-                    size={size}
-                    color={focused ? colors.white : colors.black}
-                  />
-              )
-          }
-      }}/>
-      <Screen name="Notas" component={Notas} options={{
-          tabBarIcon: ({ size, focused }) => {
-              return (
-                  <MaterialCommunityIcons
-                    name="format-annotation-plus"
-                    size={size}
-                    color={focused ? colors.white : colors.black}
-                  />
-              )
-          }
-      }}/>
-      <Screen name="Pessoas" component={Pessoas} options={{
-          tabBarIcon: ({ size, focused }) => {
-              return (
-                  <SimpleLineIcons
-                    name="people"
-                    size={size}
-                    color={focused ? colors.white : colors.black}
-                  />
-              )
-          }
-      }}/>
-    </Navigator>
+const Routes: React.FC = () => (
+
+  <NavigationContainer>
+    <Stack.Navigator
+      headerMode='none'>
+      <Stack.Screen
+        name="Login"
+        component={Login}
+      />
+      <Stack.Screen
+        name="LoggedIn"
+        component={LoggedRoutes}
+      />
+    </Stack.Navigator>
   </NavigationContainer>
+);
+
+const LoggedRoutes: React.FC = () => (
+  <Navigator
+    tabBarOptions={{
+      style: {
+        height: 70,
+        backgroundColor: colors.back,
+        borderTopWidth: 0,
+
+      },
+      tabStyle: {
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      iconStyle: {
+        flex: 0,
+        width: 20,
+        height: 20,
+      },
+      labelStyle: {
+        fontFamily: 'roboto_400',
+        fontSize: 11,
+        marginTop: 8,
+      },
+      inactiveTintColor: colors.black,
+      activeTintColor: colors.white
+    }}
+  >
+    <Screen name="Home" component={Home} options={{
+      tabBarIcon: ({ size, focused }) => {
+        return (
+          <AntDesign
+            name="home"
+            size={size}
+            color={focused ? colors.white : colors.black}
+          />
+        )
+      }
+    }}/>
+    <Screen name="Disciplinas" component={Disciplinas} options={{
+      tabBarIcon: ({ size, focused }) => {
+        return (
+          <SimpleLineIcons
+            name="graduation"
+            size={size}
+            color={focused ? colors.white : colors.black}
+          />
+        )
+      }
+    }}/>
+    <Screen name="Atividades" component={Atividades} options={{
+      tabBarIcon: ({ size, focused }) => {
+        return (
+          <AntDesign
+            name="edit"
+            size={size}
+            color={focused ? colors.white : colors.black}
+          />
+        )
+      }
+    }}/>
+    <Screen name="Notas" component={Notas} options={{
+      tabBarIcon: ({ size, focused }) => {
+        return (
+          <MaterialCommunityIcons
+            name="format-annotation-plus"
+            size={size}
+            color={focused ? colors.white : colors.black}
+          />
+        )
+      }
+    }}/>
+    <Screen name="Pessoas" component={Pessoas} options={{
+      tabBarIcon: ({ size, focused }) => {
+        return (
+          <SimpleLineIcons
+            name="people"
+            size={size}
+            color={focused ? colors.white : colors.black}
+          />
+        )
+      }
+    }}/>
+  </Navigator>
 );
 
 export default Routes;
