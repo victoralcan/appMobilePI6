6ec41a94eaf8fc3a75e69e0143872b31598d3fac
Victor Alcântara <victoralcan@gmail.com>
2020-11-18 14:25:21 -0300

    Ajustando header

--- frontend/src/styles/loggedGlobalStyle.ts
+++ frontend/src/styles/loggedGlobalStyle.ts
@@ -4,7 +4,7 @@ import colors from './colors';
 import Constants from 'expo-constants';
 
 const statusBarHeight =
-  Platform.OS === 'android' ? Constants.statusBarHeight : 0;
+  Platform.OS !== 'android' ? Constants.statusBarHeight : 0;
 
 export const Container = styled.SafeAreaView`
     padding-top: ${statusBarHeight + 'px'};  
